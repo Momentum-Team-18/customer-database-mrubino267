7ab680cc1f9fc3fac6d8c4471de0f86e12e75bbf
Meagan <meaganrubino@gmail.com>
2023-04-25 20:56:29 -0400

    semi final

--- main.js
+++ main.js
@@ -5,7 +5,14 @@ console.log(customers);
 // build a container for each customer and populate with their name
 
 for (let customer of customers) {
+    
     let customerCard = document.createElement('div');
+
+    let customerPicture = document.createElement("img");
+    customerPicture.src = customer.picture.large;
+    customerCard.appendChild(customerPicture);
+   
+    
     let customerName = document.createElement('h2');
     customerName.innerText =`${customer.name.first} ${customer.name.last}`;
     customerCard.appendChild(customerName);
@@ -20,4 +27,16 @@ for (let customer of customers) {
    customerAddress.innerText =`${customer.location.street.number} ${customer.location.street.name} ${customer.location.city}, ${ nameToAbbr (customer.location.state)}`
    customerCard.appendChild(customerAddress);
    customerProfiles.appendChild(customerCard);
+
+   let formattedDate = moment(customer.registered.date).format("MMM DD, YYYY")
+   let formattedDob = moment(customer.dob.date).format("MMM DD, YYYY")
+
+   let customerDob = document.createElement('p');
+   customerDob.innerText = `DOB: ${formattedDob} \n Customer since: ${formattedDate}`;
+   customerCard.appendChild(customerDob);
+   customerProfiles.appendChild(customerCard);
+
+   
+
 }
+
